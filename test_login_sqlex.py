2beed55389f8a61e09351f622ba2d67e1b42258b
Anton Naumenko <Naumenko_anton_pnz@mail.ru>
2025-05-26 21:36:19 +0300

    Убрал тест с неудачным поиском элемента "Anton22222" и добавил базовый успешный тест на поиск без результата.

--- test_login_sqlex.py
+++ test_login_sqlex.py
@@ -9,8 +9,6 @@ def test_login_sqlex(setup_browser):
 
 
 def test_not_find(setup_browser):
-    browser.open('https://sql-ex.ru/')
-    browser.element('[name="login"]').should(be.blank).type('Anton111111')
-    browser.element('[name="psw"]').should(be.blank).click().type('123321Anton')
-    browser.element('[name="subm1"]').click()
-    browser.element('html').should(have.text('Anton22222222'))
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('fksdfbksdbksbsdkfbsdkfkq2fqwf').press_enter()
+    browser.element('[class="card-section"]').should(have.text('По запросу fksdfbksdbksbsdkfbsdkfkq2fqwf ничего не найдено.'))
